b0cc9089a6949642e0f70a6acea7905fc8457b31
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-12-31 12:00:31 +0530

    first commit

---
+++ utilities/contractDetails.js
@@ -0,0 +1,9 @@
+import REVNU_REGISTRY from "@/contracts/deployments/inEVM/RevnuRegistry.json";
+import REVNU_TOKEN from "@/contracts/deployments/inEVM/RevnuToken.json";
+
+const DEPLOYED_CONTRACTS = {
+  REVNU_REGISTRY,
+  REVNU_TOKEN,
+};
+
+export default DEPLOYED_CONTRACTS;
